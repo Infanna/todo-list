8af091fa98565f6e9f3b9ca272326a2270a5a360
Infanna <patcharachart@odds.team>
2024-01-30 21:22:59 +0700

    [ADD]: unittest

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,41 +1,11 @@
-import { Store } from '@ngrx/store';
-import { AppComponent } from './app.component';
-import { SessionStorageService } from './core/service/session-storage.service';
-import { TokenService } from './core/service/token.service';
-import { McConfigService } from './shared/service/mc-config.service';
-import { AppState } from './store-ngrx/app.state';
-
-jest.mock('jwt-decode');
-
-const store = {
-  dispatch: jest.fn(),
-  subscribe: jest.fn(),
-};
-const session = {
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-};
-const token = {
-  getDataToken: jest.fn(),
-};
-const mcConfigService = {
-  getConfigMC: jest.fn().mockImplementation(() => ({
-    subscribe: jest.fn(),
-  })),
-};
-
-describe('AppComponent', () => {
+import { AppComponent } from "./app.component";
+describe("AppComponent", () => {
   let component: AppComponent;
   beforeEach(async () => {
-    component = new AppComponent(
-      store as unknown as Store<AppState>,
-      session as unknown as SessionStorageService,
-      token as unknown as TokenService,
-      mcConfigService as unknown as McConfigService,
-    );
+    component = new AppComponent();
   });
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     expect(component).toBeDefined();
   });
 });
--- client/src/app/component/todo-list/todo-list.component.spec.ts
+++ client/src/app/component/todo-list/todo-list.component.spec.ts
@@ -1,23 +1,75 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TodoListComponent } from "./todo-list.component";
 
-import { TodoListComponent } from './todo-list.component';
-
-describe('TodoListComponent', () => {
+describe("TodoListComponent", () => {
   let component: TodoListComponent;
-  let fixture: ComponentFixture<TodoListComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TodoListComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(TodoListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new TodoListComponent();
+    component.todoLists = [];
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should add a new todo item with the correct values when task and date form control values are provided", () => {
+    component.form.controls["task"].setValue("นอนต่อ");
+    component.form.controls["date"].setValue("31/01/2023 เวลา 09.30 น.");
+
+    component.addTodo();
+
+    expect(component.todoLists).toEqual([
+      {
+        task: "นอนต่อ",
+        date: "31/01/2023 เวลา 09.30 น.",
+        status: false,
+      },
+    ]);
+  });
+
+  it("should toggle the status of the todo item at the given index when called", () => {
+    component.todoLists = [
+      {
+        task: "ตื่นนอน",
+        date: "31/01/202 เวลา 09.00 น",
+        status: false,
+      },
+      {
+        task: "นอนต่อ",
+        date: "31/01/2023 เวลา 09.30 น",
+        status: true,
+      },
+    ];
+
+    component.updateStatus(0);
+    expect(component.todoLists[0].status).toBe(true);
+
+    component.updateStatus(1);
+    expect(component.todoLists[1].status).toBe(false);
+  });
+
+  it("should be able to delete todo in list according to the selected index", () => {
+    component.todoLists = [
+      {
+        task: "ตื่นนอน",
+        date: "31/01/2023 เวลา 09.00 น.",
+        status: false,
+      },
+      {
+        task: "แปลงฟัน",
+        date: "31/01/2023 เวลา 09.15 น.",
+        status: false,
+      },
+    ];
+
+    component.deleteTodo(0);
+
+    expect(component.todoLists).toEqual([
+      {
+        task: "แปลงฟัน",
+        date: "31/01/2023 เวลา 09.15 น.",
+        status: false,
+      },
+    ]);
   });
 });
