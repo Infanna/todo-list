966dd5ca3a6ca1f12055876ce2acc645b882d790
Infanna <patcharachart@odds.team>
2024-01-30 21:08:07 +0700

    [ADD]: add and delete todo in list

--- client/src/app/component/todo-list/todo-list.component.ts
+++ client/src/app/component/todo-list/todo-list.component.ts
@@ -1,9 +1,9 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 
 interface ITodoList {
   task: string;
   date: string;
-  time: string;
   status: boolean;
 }
 
@@ -16,9 +16,30 @@ export class TodoListComponent {
   todoLists: ITodoList[] = [
     {
       task: "ตื่นนอน",
-      date: "31/01/2023",
-      time: "09.00",
+      date: "31/01/2023 เวลา 09.00 น.",
       status: false,
     },
   ];
+
+  form = new FormGroup({
+    task: new FormControl(),
+    date: new FormControl(),
+    status: new FormControl(false),
+  });
+
+  addTodo() {
+    this.todoLists.push({
+      task: this.form.controls["task"].value || "",
+      date: this.form.controls["date"].value || "",
+      status: false,
+    });
+  }
+
+  updateStatus(index: number){
+    this.todoLists[index].status = !this.todoLists[index].status
+  }
+
+  deleteTodo(index: number) {
+    this.todoLists.splice(index, 1);
+  }
 }
